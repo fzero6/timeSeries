abcde57f88e08e2e27e0dc96d8f16336cdb6d1a7
fzero6 <fzerofsky@gmail.com>
2018-10-22 16:09:05 -0400

    cleaned up comments

--- lstm_pred.py
+++ lstm_pred.py
@@ -15,8 +15,8 @@ input_arr = [50, 0.33]
 # load the data
 X_test, X_train, y_test, y_train, max = data_load(data, input_arr)
 
-print(X_train.shape) #(17, 200, 4)
-print(y_train.shape) #(3240,)
+print(X_train.shape)
+print(y_train.shape)
 
 # Build the LSTM Model
 model = Sequential()
@@ -24,8 +24,8 @@ model.add(LSTM(128, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(64, return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Dense(16, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='relu'))
+model.add(Dense(16, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='relu'))
 
 start = time.time()
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
@@ -33,10 +33,11 @@ model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
 
 print('compilation time: ', time.time()-start, 'seconds')
-
+'''
 model.fit(
     X_train,
     y_train,
     batch_size=512,
     epochs=1,
     validation_split=0.05)
+'''
